b9e82f794b7c761cc32404c479eb8546eef915ca
hyimred <ptrk.csak@gmail.com>
2023-03-28 09:57:40 +0200

    donnee

--- src/components/pages/tapasztalat.tsx
+++ src/components/pages/tapasztalat.tsx
@@ -4,25 +4,24 @@ import petrik from "../files/petrik.jpg";
 export default class Tapasztalat extends Component {
     render() {
         return <div className="Center">
-            <div className="row">
+            <div className="flexbox">
                 
-                <div className="col-6 list">
-                    <h4>Pro-bono bulik</h4>
-                    <ul>
-                        <li>Csibeavató, 2021. 08. 31.</li>
-                        <li>Karácsonyi bál, 2021. 12. 22.</li>
-                        <li>Farsangi mulatság, 2022. 02. 20.</li>
-                    </ul>
-                    <h4>Professzionális bulik</h4>
-                    <ul>
-                        <li>Tanárkarácsony, 2021. 12. 20.</li>
-                        <li>Ballagás, 2022. 04. 30.</li>
-                    </ul>
-                </div>
+                    <div>
+                        <h4>Pro-bono bulik</h4>
+                        <ul>
+                            <li>Csibeavató, 2021. 08. 31.</li>
+                            <li>Karácsonyi bál, 2021. 12. 22.</li>
+                            <li>Farsangi mulatság, 2022. 02. 20.</li>
+                        </ul>
+                        <h4>Professzionális bulik</h4>
+                        <ul>
+                            <li>Tanárkarácsony, 2021. 12. 20.</li>
+                            <li>Ballagás, 2022. 04. 30.</li>
+                        </ul>
+                    </div>
+                
 
-                <div className="col-6">
                     <img className="" src={petrik}/>
-                </div>
             </div>
         </div>
     }
